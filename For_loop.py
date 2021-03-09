70b141039a8802b38742a927cfea3121514ac3b4
Gnoblin44 <54971953+Gnoblin44@users.noreply.github.com>
2021-03-09 22:30:19 +1100

    Add files via upload

--- For_loop.py
+++ For_loop.py
@@ -1,20 +1,25 @@
 
 animals = {'chickens': 30, 'cow': 730, 'pigs': 75, 'horse': 400, 'cockatoo': 47}
 
-shipment = ' '
+shipment = []
 shipment_weight = 0
 max_weight = 600
 
-for key, value in animals.items():
+for animal, weight in animals.items():
+    print(f'Current shipment weight: {shipment_weight}')
     
-    while True:
+    if weight + shipment_weight < max_weight:
+        print(f"Adding {animal} with weight of {weight}\n")  
+        shipment.append(animal)
+        shipment_weight += weight
+                 
+    elif weight + shipment_weight > max_weight:
+        print(   f"\nskipping {animal} with weight of {weight}\n")
+        continue
         
-        if value < max_weight:
-            print(f'adding {key} to the shipment')
-            key += shipment
-            value += shipment_weight
-        
-        if value > max_weight:
-            print('You have reached the weight limit')
-            continue 
-        
+    else: 
+        print("You have passed the weight limit")
+        break
+    
+print(f"\n Final Shipment Weight {shipment_weight}")
+print(f" Final Shipment {shipment}")
